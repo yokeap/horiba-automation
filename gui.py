1957837232f8c7ecea161aaf47c3bdac23df81e9
yokeap <siwakorn.ictrl@gmail.com>
2024-10-12 14:28:33 +0700

    several update

--- gui.py
+++ gui.py
@@ -1,14 +1,17 @@
+import os
+import subprocess
 import sys
 import autoit
 import time
 import pyautogui
 import threading
+import pyuac
 from datetime import datetime
 
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QMainWindow
 
-
+stop_event = threading.Event()
 
 # app = QApplication(sys.argv)
 
@@ -29,34 +32,59 @@ from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWid
 class Mesa():
     def __init__(self):
         super().__init__()
+        self.init = True
         # bootup process
         time.sleep(10) 
         try:
             pyautogui.click('./imgdata/_0_btCancel.png')
         except pyautogui.ImageNotFoundException:
             print('Error: Start program')
-            # send error to arduino
+            self.init = False
 
-    def process(self):
-        
         ################ project open process ###################
-        time.sleep(1) 
+        time.sleep(1)
         try:
             pyautogui.hotkey('ctrl', 'o')
             time.sleep(1) 
             pyautogui.click('./imgdata/_1_ltProjectOpen.png', clicks=2)
         except pyautogui.ImageNotFoundException:
             print('Error: project open')
+            self.init = False
         
+            # send error to arduino
 
+    def process(self,stop_event):
+        
         ################# run egat cal curve ####################
         time.sleep(2) 
         try:
-            pyautogui.click('./imgdata/_2_btEGATCal.png')
-            time.sleep(300) 
+            pyautogui.click('./imgdata/_2_btEGATCal.png')       
             # runState = pyautogui.locateOnScreen('calc7key.png')
         except pyautogui.ImageNotFoundException:
             print('Error: cannot run')
+            return False
+
+        ################# check success #######################
+        time.sleep(10) 
+        elapsed = 0
+        start = time.time()
+        flagSuccess = False
+        while elapsed < 300:
+            try:
+                # time.sleep(10) 
+                done = time.time()
+                elapsed = done - start
+                ready = pyautogui.locateOnScreen('./imgdata/_2_1_ready.png')
+                if ready:
+                    elapsed = 310
+                    flagSuccess = True              
+            except pyautogui.ImageNotFoundException:
+                print('Wait for process.')
+
+        if(flagSuccess == False):
+            print("incompleted")
+            return False
+
 
         ################## report process #######################
         time.sleep(1) 
@@ -64,6 +92,7 @@ class Mesa():
             pyautogui.click('./imgdata/_3_btReport.png')
         except pyautogui.ImageNotFoundException:
             print('Error: cannot report')
+            return False
 
         ################## save process #########################
         time.sleep(3) 
@@ -71,16 +100,21 @@ class Mesa():
             pyautogui.click('./imgdata/_4_btExport.png')
             time.sleep(1) 
             pyautogui.click('./imgdata/_5_ddExcel.png')
-            autoit.win_wait_active("[CLASS:Save As]", 3)
-            autoit.control_send("[CLASS:Save As]", 1001, "D:\SaveTemp")
+            autoit.win_wait_active("Save As", 3)
+            time.sleep(1) 
+            # autoit.control_click("Save As", "Address Band Root1")
+            # autoit.control_send("Save As", "Address Band Root1", "D:\Data")
             saveDT = "Report" + datetime.now().strftime("%d-%m-%Y--%H-%M-%S")
-            autoit.control_send("[CLASS:Save As]", "Edit", saveDT)
-            autoit.control_click("[CLASS:Save As]", "Button2")
-        except pyautogui.ImageNotFoundException:
-            print('Error: cannot export')
+            autoit.control_send("Save As", "Edit1", saveDT)
+            time.sleep(1) 
+            autoit.control_click("Save As", "Button2")
+        except pyautogui.ImageNotFoundException as e:
+            print(e)
+            return False
 
-        except autoit.autoit.AutoItError:
-            print('Error: cannot save')
+        except autoit.autoit.AutoItError as e:
+            print(e)
+            return False
 
         ##################### close ############################
         time.sleep(1) 
@@ -88,10 +122,14 @@ class Mesa():
             pyautogui.click('./imgdata/_6_btClose.png')
         except pyautogui.ImageNotFoundException:
             print('Error: cannot report')
+            return False
     
     def run(self, stop_event):
         while not stop_event.is_set():
-            self.process()
+            if self.process(stop_event) == False:
+                stop_event.set()
+                print("Thread stop!")
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -134,24 +172,31 @@ class MainWindow(QWidget):
   
     # action method 
     def clickStart(self): 
-  
-        # printing pressed 
-        # autoit.run("MESA.exe")
+        os.chdir(r"C:\Program Files (x86)\HORIBA\HORIBA X-RAY LAB For MESA-50 SERIES")
+        subprocess.Popen("MesaApplication.exe")
+        os.chdir(r"D:\software\horiba-automation-main")
         self.mesaApp = Mesa()
-        self.stop_event = threading.Event()
-        self.mesaThread = threading.Thread(target=self.mesaApp.run, args=(self.stop_event,))
-
-        self.mesaThread.start()
-        print("pressed") 
+        if self.mesaApp.init == True:
+            self.stop_event = threading.Event()
+            self.mesaThread = threading.Thread(target=self.mesaApp.run, args=(self.stop_event,))
+            self.mesaThread.daemon = True
+            self.mesaThread.start()
+        else:
+            print("Mesa Initilization fail")
 
     def clickStop(self):
         # self.mesaThread.terminate() 
-        self.stop_event.set()
+        stop_event.set()
+        sys.exit()
         # printing pressed 
-        # print("pressed") 
+        print("stop thread") 
 
 
 if __name__ == "__main__":
+    # if not pyuac.isUserAdmin():
+    #     print("Re-launching as admin!")
+    #     pyuac.runAsAdmin()
+    # else:
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
