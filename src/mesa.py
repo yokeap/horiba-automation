39db92cbc940a1199c1ece595d8b4a5e8bacedf6
yokeap <siwakorn.ictrl@gmail.com>
2024-10-21 21:27:23 +0700

    some update to mesa.py

--- src/mesa.py
+++ src/mesa.py
@@ -1,34 +1,68 @@
-class Mesa():
-    def __init__(self):
-        super().__init__()
+import autoit
+import pyautogui
+import time
+import threading
+from datetime import datetime
 
-    def process(self):
+class mesa:
+    def __init__(self):
+        # super().__init__()
+        self.stop_event = threading.Event()
         # bootup process
         time.sleep(10) 
         try:
             pyautogui.click('./imgdata/_0_btCancel.png')
         except pyautogui.ImageNotFoundException:
             print('Error: Start program')
-            # send error to arduino
+            self.init = False
 
         ################ project open process ###################
-        time.sleep(1) 
+        time.sleep(1)
         try:
             pyautogui.hotkey('ctrl', 'o')
             time.sleep(1) 
             pyautogui.click('./imgdata/_1_ltProjectOpen.png', clicks=2)
         except pyautogui.ImageNotFoundException:
             print('Error: project open')
+            self.init = False
+    
+        self.init = True
+        self.mesaThread = threading.Thread(target=self.run)
+        self.mesaThread.daemon = True
         
 
+    def process(self):
+        
         ################# run egat cal curve ####################
         time.sleep(2) 
         try:
-            pyautogui.click('./imgdata/_2_btEGATCal.png')
-            time.sleep(300) 
+            pyautogui.click('./imgdata/_2_btEGATCal.png')       
             # runState = pyautogui.locateOnScreen('calc7key.png')
         except pyautogui.ImageNotFoundException:
             print('Error: cannot run')
+            return False
+
+        ################# check success #######################
+        time.sleep(10) 
+        elapsed = 0
+        start = time.time()
+        flagSuccess = False
+        while elapsed < 300:
+            try:
+                # time.sleep(10) 
+                done = time.time()
+                elapsed = done - start
+                ready = pyautogui.locateOnScreen('./imgdata/_2_1_ready.png')
+                if ready:
+                    elapsed = 310
+                    flagSuccess = True              
+            except pyautogui.ImageNotFoundException:
+                print('Wait for process.')
+
+        if(flagSuccess == False):
+            print("incompleted")
+            return False
+
 
         ################## report process #######################
         time.sleep(1) 
@@ -36,6 +70,7 @@ class Mesa():
             pyautogui.click('./imgdata/_3_btReport.png')
         except pyautogui.ImageNotFoundException:
             print('Error: cannot report')
+            return False
 
         ################## save process #########################
         time.sleep(3) 
@@ -43,16 +78,21 @@ class Mesa():
             pyautogui.click('./imgdata/_4_btExport.png')
             time.sleep(1) 
             pyautogui.click('./imgdata/_5_ddExcel.png')
-            autoit.win_wait_active("[CLASS:Save As]", 3)
-            autoit.control_send("[CLASS:Save As]", 1001, "D:\SaveTemp")
+            autoit.win_wait_active("Save As", 3)
+            time.sleep(1) 
+            # autoit.control_click("Save As", "Address Band Root1")
+            # autoit.control_send("Save As", "Address Band Root1", "D:\Data")
             saveDT = "Report" + datetime.now().strftime("%d-%m-%Y--%H-%M-%S")
-            autoit.control_send("[CLASS:Save As]", "Edit", saveDT)
-            autoit.control_click("[CLASS:Save As]", "Button2")
-        except pyautogui.ImageNotFoundException:
-            print('Error: cannot export')
+            autoit.control_send("Save As", "Edit1", saveDT)
+            time.sleep(1) 
+            autoit.control_click("Save As", "Button2")
+        except pyautogui.ImageNotFoundException as e:
+            print(e)
+            return False
 
-        except autoit.autoit.AutoItError:
-            print('Error: cannot save')
+        except autoit.autoit.AutoItError as e:
+            print(e)
+            return False
 
         ##################### close ############################
         time.sleep(1) 
@@ -60,3 +100,16 @@ class Mesa():
             pyautogui.click('./imgdata/_6_btClose.png')
         except pyautogui.ImageNotFoundException:
             print('Error: cannot report')
+            return False  
+
+    def run(self):
+        while not self.stop_event.is_set():
+            if self.process() == False:
+                self.stop_event.set()
+                print("Thread stop!") 
+
+    def start_thread(self):        
+        self.mesaThread.start()
+
+    def stop_thread(self):
+        self.stop_event.set()
